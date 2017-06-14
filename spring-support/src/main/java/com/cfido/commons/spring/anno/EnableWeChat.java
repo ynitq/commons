92e390368417ca6033dfc4b10922c9398c8d86bb
梁韦江 <liang.weijiang@lin-zi.com>
2017-06-14 10:48:10 +0800

    微信服务关联redis

--- spring-support/src/main/java/com/cfido/commons/spring/anno/EnableWeChat.java
+++ spring-support/src/main/java/com/cfido/commons/spring/anno/EnableWeChat.java
@@ -8,21 +8,23 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Import;
 
-import com.cfido.commons.spring.weChat.WeChatConfig;
+import com.cfido.commons.spring.redis.RedisConfig;
+import com.cfido.commons.spring.weChat.WeChatAutoConfig;
 import com.cfido.commons.spring.weChat.WeChatProperties;
 
 /**
- * 微信接入服务
+ * 微信接入服务, 微信的accessToken保存在 redis中，所以必须有redis的配置
  * 
- * @author 梁韦江
- *  2016年8月11日
+ * @author 梁韦江 2016年8月11日
  * 
  * @see WeChatProperties 可配置参数
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Import(WeChatConfig.class)
+@Import(value = {
+		WeChatAutoConfig.class, RedisConfig.class
+})
 public @interface EnableWeChat {
 
 }
