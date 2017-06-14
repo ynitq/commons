1e2b9078f9659641cadb7584656a5bf689c50eff
梁韦江 <liang.weijiang@lin-zi.com>
2017-06-14 10:47:41 +0800

    修改帮助文字

--- spring-support/src/main/java/com/cfido/commons/spring/sendMail/SendMailAutoConfig.java
+++ spring-support/src/main/java/com/cfido/commons/spring/sendMail/SendMailAutoConfig.java
@@ -3,19 +3,15 @@ package com.cfido.commons.spring.sendMail;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.cfido.commons.spring.debugMode.DebugModeProperties;
-
 /**
  * <pre>
- * SendMailAutoConfig自动化配置
+ * SendMailAutoConfig自动化配置，配置完成后，可使用{@link SendMailService}服务，用于发送邮件
  * </pre>
- * 
- * @see DebugModeProperties 其实就在自动创建这个叫DebugModeProperties的bean
  * 
  * @author 梁韦江 2016年8月26日
  */
 @Configuration
-@ComponentScan(basePackageClasses = SendMailAccountPool.class)
+@ComponentScan(basePackageClasses = SendMailAutoConfig.class)
 public class SendMailAutoConfig {
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SendMailAutoConfig.class);
 
