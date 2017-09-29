b0997aba502e4c497146c7103871f134e9ba919c
juncong.li <1281162287@qq.com>
2017-09-29 21:03:37 +0800

    timekeyutil加上获取hour

--- utils/src/main/java/com/cfido/commons/utils/utils/TimeKeyUtil.java
+++ utils/src/main/java/com/cfido/commons/utils/utils/TimeKeyUtil.java
@@ -29,16 +29,23 @@ public class TimeKeyUtil {
 		int day = c.get(Calendar.DAY_OF_MONTH);
 		int hour = c.get(Calendar.HOUR_OF_DAY);
 
-		return ((year * 100 + mon) * 100 + day) + hour;
+		return (year * 100 + mon) * 100 + day + hour;
 	}
 
-	/** 获取当前的小时的key */
+	/** 获取当前的月的key */
 	public static int getMonthKey() {
 		Calendar c = Calendar.getInstance();
 		int year = c.get(Calendar.YEAR);
 		int mon = c.get(Calendar.MONTH) + 1;
 
 		return year * 100 + mon;
+	}
+
+	/** 获取当前小时 */
+	public static int getHour() {
+		Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		return hour;
 	}
 
 }
