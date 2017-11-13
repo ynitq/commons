005bd14c0a4c76c0e7cc74932228bb2c9312948e
juncong.li <1281162287@qq.com>
2017-11-13 14:38:47 +0800

    修改baseApiException

--- beans/src/main/java/com/cfido/commons/beans/apiServer/BaseApiException.java
+++ beans/src/main/java/com/cfido/commons/beans/apiServer/BaseApiException.java
@@ -1,5 +1,7 @@
 package com.cfido.commons.beans.apiServer;
 
+import com.cfido.commons.annotation.api.ADataInApiException;
+
 /**
  * api抛错的基类，必须有错误代码和错误信息
  * 
@@ -7,6 +9,7 @@ package com.cfido.commons.beans.apiServer;
  * 
  */
 public abstract class BaseApiException extends Exception {
+
 	private static final long serialVersionUID = 1L;
 
 	public BaseApiException() {
@@ -23,22 +26,32 @@ public abstract class BaseApiException extends Exception {
 
 	public BaseApiException(String paramString) {
 		super(paramString);
+		this.errorMsg = paramString;
+	}
+
+	public BaseApiException(String paramString, Integer messageCode) {
+		this(paramString);
+		this.messageCode = messageCode;
 	}
 
 	public BaseApiException(Throwable paramThrowable) {
 		super(paramThrowable);
 	}
 
+	private String errorMsg;
+
 	/**
 	 * 错误的提示消息
 	 * 
 	 * @return 错误提示
 	 */
-	public abstract String getErrorMsg();
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
 
 	@Override
 	public String getMessage() {
-		return this.getErrorMsg();
+		return getErrorMsg();
 	}
 
 	/**
@@ -48,6 +61,17 @@ public abstract class BaseApiException extends Exception {
 	 */
 	public int getHttpStatusCode() {
 		return 200;
+	}
+
+	@ADataInApiException
+	protected Integer messageCode;
+
+	public Integer getMessageCode() {
+		return messageCode;
+	}
+
+	public void setMessageCode(Integer messageCode) {
+		this.messageCode = messageCode;
 	}
 
 }
